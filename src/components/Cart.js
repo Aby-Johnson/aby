33e96634f57ef2f1e4be8bd0ec49a18e97a8ec73
Aby Johnson <aby@Abys-iMac.local>
2020-02-05 18:31:20 +0530

    FirstLook

---
+++ src/components/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Cart extends React.Component {
+	
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div> Hello from cart</div>
+		);
+	}
+}
---
+++ src/components/Default.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Default extends React.Component {
+
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>Page Note Found!</div>
+		);
+	}
+}
